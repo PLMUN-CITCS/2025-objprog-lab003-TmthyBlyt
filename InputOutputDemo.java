91d168514a1b70190fd7119b93eb726fb2e3adbe
TmthyBlyt <baluyottimothyjohn_bscs@plmun.edu.ph>
2025-02-14 16:47:25 +0800

    Submitting OBJPROG Week 02 - Session 01 - Exercise 02

--- InputOutputDemo.java
+++ InputOutputDemo.java
@@ -9,6 +9,7 @@ double userDouble = input.nextDouble();
 input.nextLine(); // Consume newline
 System.out.print("Enter a word or sentence: ");
 String userString = input.nextLine();
+System.out.printf("Your integer: %d%n", userInt);
 System.out.printf("Your decimal number: %.2f%n", userDouble);
 System.out.printf("Your string: %s%n", userString);
 input.close();
